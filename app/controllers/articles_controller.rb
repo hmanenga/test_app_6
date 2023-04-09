f0adeb9a11e9fa9894a2fcc45ca29094677d539c
hmanenga <hendrick.manenga@gmail.com>
2023-04-09 07:26:05 +0100

    Add edit and update actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show edit update destroy ]
+  #before_action :set_article, only: %i[ show edit update destroy ]
 
   # GET /articles or /articles.json
   def index
@@ -10,6 +10,37 @@ class ArticlesController < ApplicationController
   # GET /articles/1 or /articles/1.json
   def show
     @article= Article.find(params[:id])
+  end
+
+  #GET  /articles/new(.:format)
+  def new
+    @article=Article.new
+  end
+
+   # /articles(.:format)
+  def create
+    @article=Article.new(params.require(:article).permit(:title,:description))
+    if @article.save
+      flash[:notice]="Article was created successfuly."
+      redirect_to articles_path(@article)
+    else 
+        render 'new'
+    end
+      #render plain: @article.errors.full_messages    
+  end
+
+  def edit
+    @article=Article.find(params[:id])
+  end
+  
+  def update
+    @article=Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title,:description));
+      flash[:notice]='Article updated successfully'
+      redirect_to @article
+    else 
+      render 'edit'
+    end
   end
 
 end
