a5ede776089f91d5b74d4d019f9dae2e00c124cb
hmanenga <hendrick.manenga@gmail.com>
2023-04-10 09:03:23 +0100

    Refactor code using partials and make app production ready

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
-  #before_action :set_article, only: %i[ show edit update destroy ]
-
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+ 
+  
   # GET /articles or /articles.json
   def index
     #binding.break
@@ -9,7 +10,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1 or /articles/1.json
   def show
-    @article= Article.find(params[:id])
+ 
   end
 
   #GET  /articles/new(.:format)
@@ -19,7 +20,7 @@ class ArticlesController < ApplicationController
 
    # /articles(.:format)
   def create
-    @article=Article.new(params.require(:article).permit(:title,:description))
+    @article=Article.new(article_params)
     if @article.save
       flash[:notice]="Article was created successfuly."
       redirect_to articles_path(@article)
@@ -30,12 +31,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article=Article.find(params[:id])
   end
   
   def update
-    @article=Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title,:description));
+    if @article.update(article_params);
       flash[:notice]='Article updated successfully'
       redirect_to @article
     else 
@@ -43,4 +42,16 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+  private
+  def set_article
+    @article= Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title,:description)
+  end
 end
